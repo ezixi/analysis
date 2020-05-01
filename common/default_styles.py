5fe47ea9416bb1a1811a8648a3f3dca62431c1f2
Martin Bell <martinb@stonecroptech.com>
2020-05-01 16:37:19 +0100

    formatting

---
+++ common/default_styles.py
@@ -0,0 +1,22 @@
+# import pandas as pd
+# import numpy as np
+import matplotlib.pyplot as plt
+
+
+class DefaultStyles:
+
+    plt.style.use("fivethirtyeight")
+
+    def legible_header(self, df):
+        """
+        Removes non-postgressy headers from a data frame
+        """
+        df.columns = (
+            df.columns.str.strip()
+            .str.lower()
+            .str.replace("_", " ")
+            .str.replace("(", "")
+            .str.replace(")", "")
+            .str.replace("#", "number")
+        )
+        return df
