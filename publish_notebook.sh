3a29b82dd4ccdef1edc9beb2a47e0e45eb916584
Martin Bell <martinb@stonecroptech.com>
2020-05-07 14:45:15 +0100

    updates hook

--- publish_notebook.sh
+++ publish_notebook.sh
@@ -2,5 +2,5 @@
 
 if [[ $1 == *.ipynb ]]; then
     fdname=$(dirname "$1" .ipynb)
-    jupyter nbconvert --output-dir="$fdname" "$1"
+    jupyter nbconvert --template basic --output-dir="../$fdname" "$1"
 fi
